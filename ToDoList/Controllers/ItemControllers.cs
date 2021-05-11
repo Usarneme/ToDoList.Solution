870eb73717144d2cb4076c21879c6c25f774ef0b
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-11 15:08:34 -0600

    solved error for index.cshtml
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- ToDoList/Controllers/ItemControllers.cs
+++ ToDoList/Controllers/ItemControllers.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
   public class ItemController : Controller
   {
 
-    [Route("/items")]
+    [HttpGet("/items")]
     public ActionResult Index()
     {
       // contact the model and get all the current items in the todo list
-      return View();
+      List<ToDoList.Models.Item> allTodos = ToDoList.Models.Item.GetAll();
+      return View(allTodos);
+    }
+
+    [HttpPost("/items")]
+    public ActionResult HandleNewToDo(string new_item)
+    {
+      // call our model to create the new todo item
+      Item newItem = new Item(new_item);
+      // the Item model then saves it to _instances
+      // redirect back to get the items page
+      return RedirectToAction("Index");
+
     }
 
   }
