d871fdf8538e2e1e3e88a55d0dfbaed7b8db2df4
Usarneme <jamestcoop@gmail.com>
2021-05-17 21:07:56 -0700

    WIP - updates views and controllers to use entity framework; style adjustments

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -1,11 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
+
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Item> model = _db.Items.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
     // [HttpGet("/categories/{categoryId}/items/{itemId}")]
     // public ActionResult Show(int categoryId, int itemId)
     // {
@@ -17,18 +51,18 @@ namespace ToDoList.Controllers
     //   return View(model);
     // }
 
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
-    {
-      Category category = Category.Find(categoryId);
-      return View(category);
-    }
+    // [HttpGet("/categories/{categoryId}/items/new")]
+    // public ActionResult New(int categoryId)
+    // {
+    //   Category category = Category.Find(categoryId);
+    //   return View(category);
+    // }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
+    // [HttpPost("/items/delete")]
+    // public ActionResult DeleteAll()
+    // {
+    //   Item.ClearAll();
+    //   return View();
+    // }
   }
 }
