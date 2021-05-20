5acc578b04ff63d712ef0271423d95b00d9a0cb7
Usarneme <jamestcoop@gmail.com>
2021-05-20 14:51:49 -0700

    updates views to correspond with controllers/route handlers

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -5,15 +5,6 @@ namespace ToDoList.Controllers
   public class HomeController : Controller
   {
 
-    [HttpGet("/hello")]
-    public string Hello() { return "Hello friend!"; }
-
-    [Route("/goodbye")]
-    public ActionResult GoodBye() { return View(); }
-
-    // [Route("/")]
-    // public string Letter() { return "Our virtual To Do List will go here soon!"; }
-
     [Route("/")]
     public ActionResult Index() { return View(); }
 
--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -73,29 +73,5 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
-    // [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    // public ActionResult Show(int categoryId, int itemId)
-    // {
-    //   Item item = Item.Find(itemId);
-    //   Category category = Category.Find(categoryId);
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   model.Add("item", item);
-    //   model.Add("category", category);
-    //   return View(model);
-    // }
-
-    // [HttpGet("/categories/{categoryId}/items/new")]
-    // public ActionResult New(int categoryId)
-    // {
-    //   Category category = Category.Find(categoryId);
-    //   return View(category);
-    // }
-
-    // [HttpPost("/items/delete")]
-    // public ActionResult DeleteAll()
-    // {
-    //   Item.ClearAll();
-    //   return View();
-    // }
   }
 }
